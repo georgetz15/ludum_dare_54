7f5cf4744f0eef4398180768e374311a603bc770
George Tzoupis <george.tzoupis@gmail.com>
2023-09-30 14:51:05 +0300

    added graph creation

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -1,18 +1,76 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
+    private Dictionary<Transform, HashSet<Transform>> _pathGraph;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        var distance = 5;
+        _pathGraph = GeneratePathGraph(distance, LayerMask.GetMask("Planets"));
+        var edges = new List<Tuple<Transform, Transform>>();
+        foreach (var nodeFrom in _pathGraph.Keys)
+        {
+            foreach (var nodeTo in _pathGraph[nodeFrom])
+            {
+                edges.Add(new Tuple<Transform, Transform>(nodeFrom, nodeTo));
+            }
+        }
+        // TODO: render the graph
+        foreach (var edge in edges)
+        {
+            Debug.Log($"Found edge {edge.Item1.name}, {edge.Item2.name}");
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+    }
+
+    // Planets with distance within range are connected
+    private static Dictionary<Transform, HashSet<Transform>> GeneratePathGraph(float distance, LayerMask nodesLayer)
+    {
+        // Hardcode this to sth big
+        const float vertexSearchDistance = 1000f;
+        var hitColliders = new List<Collider>(
+            Physics.OverlapSphere(Vector3.zero,
+                vertexSearchDistance,
+                nodesLayer
+            ));
+
+        var graph = new Dictionary<Transform, HashSet<Transform>>();
+        foreach (var collider in hitColliders) graph.Add(collider.transform, new HashSet<Transform>());
+
+        var visited = new HashSet<Transform>();
+        var newNodes = new Stack<Transform>(graph.Keys);
+
+        while (newNodes.Count > 0)
+        {
+            var currentNode = newNodes.Pop();
+            visited.Add(currentNode);
+
+            // Find new nodes
+            hitColliders = new List<Collider>(
+                Physics.OverlapSphere(currentNode.position,
+                    distance,
+                    nodesLayer));
+            foreach (var otherNode in
+                     hitColliders
+                         .Select(hitCollider => hitCollider.transform)
+                         .Where(transform => !visited.Contains(transform)))
+            {
+                graph[currentNode].Add(otherNode);
+                // Keep graph "directional" to simplify things
+                // when rendering edges
+                // graph[otherNode].Add(currentNode);
+            }
+        }
+
+        return graph;
     }
 }
