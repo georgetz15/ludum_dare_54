1248d2101de8530f9a337f9d7c68b89743bd99a5
klados <klados@192.168.1.7>
2023-09-30 20:24:15 +0300

    planets names

--- Assets/Scripts/PlanetSpawner.cs
+++ Assets/Scripts/PlanetSpawner.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using Random = UnityEngine.Random;
 
 public class PlanetSpawner : MonoBehaviour
 {
@@ -9,6 +11,28 @@ public class PlanetSpawner : MonoBehaviour
 	public int numPlanets = 6; // Number of planets to generate
 	public Vector2 gridSize = new Vector2(20f, 20f); // Size of the grid
 	public Vector2 initialPosition = Vector2.zero; // Initial position to start generating planets	
+	private string[] _planetNames = {
+		"Mercury",
+		"Venus",
+		"Earth",
+		"Mars",
+		"Jupiter",
+		"Saturn",
+		"Uranus",
+		"Neptune",
+		"Pluto",
+		"Ceres",
+		"Eris",
+		"Haumea",
+		"Makemake",
+		"Planet Nine",
+		"Kepler-452b",
+		"Gliese 581g",
+		"HD 209458b",
+		"Tatooine",
+		"Alderaan",
+		"Endor"
+	};
 	
 	// Size controls
 	public float minSize = 1f;
@@ -38,13 +62,15 @@ public class PlanetSpawner : MonoBehaviour
 		// Create a list of positions to track where planets are placed
 		Vector3 currentPosition = initialPosition;
 
+		int planetNameCounter = 0;
 		for (int i = 0; i < numPlanets; i++)
 		{
 			// Randomly choose a planet prefab from the array
 			GameObject randomPlanetPrefab = planetPrefabs[Random.Range(0, planetPrefabs.Length)];
 
 			GameObject planet = Instantiate(randomPlanetPrefab, currentPosition, Quaternion.identity);
-			planet.name = $"Planet_{i}";
+			planetNameCounter = i%_planetNames.Length;
+			planet.name = $"Planet_{_planetNames[planetNameCounter]}";
 
 			// Set random size, animation speed, and cycle offset
 			float randomSize = Random.Range(minSize, maxSize);
