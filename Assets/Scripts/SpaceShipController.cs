d6254326636787e922343ab77e94e26c6136dc6a
George Tzoupis <george.tzoupis@gmail.com>
2023-10-01 11:47:31 +0300

    added travel logic

--- Assets/Scripts/SpaceShipController.cs
+++ Assets/Scripts/SpaceShipController.cs
@@ -10,6 +10,11 @@ public class SpaceShipController : MonoBehaviour
     [SerializeField] private UnityEvent onCargoChanged;
     private int _cargo;
 
+    [SerializeField] private float flyingSpeed = 3.0f;
+    public bool IsTravelling { get; private set; }
+    private Transform _destination;
+    [SerializeField] private UnityEvent<Transform> onTravelFinished = new();
+
     public int MaxCargoCapacity
     {
         get => maxCargoCapacity;
@@ -37,11 +42,14 @@ public class SpaceShipController : MonoBehaviour
 
     public void TravelTo(GameObject destination)
     {
-        transform.position = destination.transform.position;
-        CurrentPlanet = destination;
+        if (destination == CurrentPlanet) return;
+        
+        // transform.position = destination.transform.position;
+        _destination = destination.transform;
+        transform.up = _destination.position - transform.position;
+        IsTravelling = true;
     }
-
-
+    
     // Start is called before the first frame update
     private void Awake()
     {
@@ -49,11 +57,25 @@ public class SpaceShipController : MonoBehaviour
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
+        if (!IsTravelling) return;
+        
+        var step = flyingSpeed * Time.fixedDeltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, _destination.position, step);
+
+        // Arrived
+        if (Vector3.Distance(transform.position, _destination.position) < 0.001f)
+        {
+            CurrentPlanet = _destination.gameObject;
+            IsTravelling = false;
+            _destination = null;
+
+            onTravelFinished.Invoke(_destination);
+        }
     }
 
-    public void setRandomPlanetStartingPoint()
+    public void SetRandomPlanetStartingPoint()
     {
         var planets = GameController.GetObjectsInLayer(LayerMask.GetMask("Planets")).ToList();
         var i = Random.Range(0, planets.Count - 1);
